1dd229b8346817fc5dd1a808dbc27cab12fbf814
ESPhoenix <e.s.phoenix@hotmail.co.uk>
2025-04-02 15:05:11 +0100

    Protocol_4_Charging || CLEAN CODE and files split up

---
+++ Laboratory/Experiments/Protocol_4_Charging/Charged_Assistant.py
@@ -0,0 +1,176 @@
+## BASIC IMPORTS ##
+from subprocess import call, DEVNULL 
+from os import path as p
+import os
+from shutil import  copy
+import pandas as pd
+import numpy as np
+
+## drFrankenstein LIBRARIES ##
+from OperatingTools import drOrca
+
+## CLEAN CODE ##
+class FilePath:
+    pass
+class DirectoryPath:
+    pass
+
+
+# 🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲
+def generate_charge_constraints_file(config: dict, outDir: DirectoryPath) -> FilePath:
+    """
+    Uses charge groups from config to generate charge constraints file to be used by MultiWFN
+
+    Args:
+        config (dict): config containing all run information
+        outDir (DirectoryPath): path to output directory
+
+    Returns:
+        chargeConstraintsTxt (FilePath): path to charge constraints file
+    
+    """
+    ## unpack config
+    chargeGroups: dict = config["moleculeInfo"]["chargeGroups"]
+
+
+    chargeConstraintsTxt = p.join(outDir, "charge_group_constraints.txt")
+    with open(chargeConstraintsTxt, "w") as f:
+        for _, chargeGroupData in chargeGroups.items():
+            indexes = chargeGroupData["indexes"]
+            if len(indexes) == 0:
+                continue
+            charge = chargeGroupData["charge"]
+            formattedIndexes = ",".join(map(str, indexes))
+            f.write(f"{formattedIndexes} {str(charge)}\n")
+          
+
+    return chargeConstraintsTxt
+# 🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲
+def generate_conformer_list_file(orcaCalculationsDir: DirectoryPath,
+                                  chargeFittingDir: DirectoryPath) -> FilePath:
+    """
+    Reads through single point calculations for each conformer 
+    and gets the single-point energies
+    converts these into Boltzmann probabilities 
+    and creates a conformer_list.txt file for MultiWFN
+
+    Args:
+        orcaCalculationsDir (DirectoryPath): path to dir containing single point calculations
+        chargeFittingDir (DirectoryPath): path to dir for charge fitting calculations
+
+    Returns:
+        conformerListTxt (FilePath): path to conformer list file
+    """
+
+    singlePointData = {}
+    for conformerName in os.listdir(orcaCalculationsDir):
+        singlePointData[conformerName] = {}
+        calculationDir = p.join(orcaCalculationsDir, conformerName)
+        if not p.isdir:
+            continue
+        singlePointOutFile = p.join(calculationDir, "orca_sp.out")
+        singlePointEnergy = find_final_single_point_energy(singlePointOutFile)
+        singlePointData[conformerName]["Energy"] = singlePointEnergy
+        singlePointData[conformerName]["Path"] = p.join(chargeFittingDir, f"{conformerName}.molden.input")
+
+
+    singlePointDf =pd.DataFrame.from_dict(singlePointData, orient='index')
+
+    singlePointDf = calculate_boltzmann_probabilities(singlePointDf)
+
+    conformerListTxt = p.join(chargeFittingDir, "conformer_list.txt")
+    with open(conformerListTxt, "w") as f:
+        for _, row in singlePointDf.iterrows():
+            f.write(f"{row['Path']} {str(row['Probability'])}\n")
+
+    return conformerListTxt
+
+# 🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲
+def calculate_boltzmann_probabilities(df, temperature=298.15):
+    boltzmannConstant = 0.0083144621  # kJ/(mol*K)
+    energies = df['Energy'].values
+    minEnergy = np.min(energies)
+    deltaEnergies = energies - minEnergy
+    boltzmannFactors = np.exp(-deltaEnergies / 
+                              (boltzmannConstant * temperature))
+    probabilities = boltzmannFactors / np.sum(boltzmannFactors)
+    df['Probability'] = probabilities
+    return df    
+
+# 🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲
+def find_final_single_point_energy(outFilePath):
+    with open(outFilePath, 'r') as file:
+        for line in file:
+            if "FINAL SINGLE POINT ENERGY" in line:
+                return float(line.split()[-1])
+
+
+# 🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲
+def apply_resp2_weighted_average(solvatedDf: pd.DataFrame,
+                                  gasPhaseDf: pd.DataFrame,
+                                    proportions: list[float] = [0.6, 0.4]) -> pd.DataFrame:
+    """
+    Takes charges that have been calculated with and without solvation
+    and applies a weighted average to create RESP2 charges
+
+    Args:
+        solvatedDf (pd.DataFrame): DataFrame of charges calculated with solvation
+        gasPhaseDf (pd.DataFrame): DataFrame of charges calculated in the gas phase
+        proportions (list, optional): List of proportions to apply to solvatedDf and gasPhaseDf. Defaults to [0.6, 0.4]
+
+    Returns:
+       resp2Df (pd.DataFrame): DataFrame of RESP2 charges
+    """
+
+    resp2Df = pd.DataFrame()
+    resp2Df[["atomIndex","atomElement"]] = solvatedDf[["atomIndex","atomElement"]]
+    resp2Df["Charge"] = proportions[0] * solvatedDf["Charge"] + proportions[1] * gasPhaseDf["Charge"]
+    return resp2Df
+# 🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲
+def set_up_directories(config: dict, protocol: str) -> dict:
+    """
+    Creates a directory structure for charge calculations
+    The structure depends on whether RESP or RESP2 is being run
+
+    Args:
+        config (dict): Dictionary containing all information needed
+        protocol (str): either RESP or RESP2
+
+    Returns:
+        config (dict): Updated config dictionary
+    """
+    ## get pathInfo dict
+    pathInfo: dict = config["pathInfo"]
+
+    outputDir: DirectoryPath = pathInfo["outputDir"]
+    ## charge dir - acts as a topDir for all charge-related processes
+    chargeDir: DirectoryPath = p.join(outputDir, "04_charge_calculations")
+    os.makedirs(chargeDir, exist_ok=True)
+
+    #### FOR RESP FITTING, just run once ##
+    if protocol == "RESP":
+        orcaCalculationsDir: DirectoryPath = p.join(chargeDir, "orca_calculations")
+        os.makedirs(orcaCalculationsDir, exist_ok=True)
+        chargeFittingDir: DirectoryPath = p.join(chargeDir, "charge_fitting")
+        os.makedirs(chargeFittingDir, exist_ok=True)
+        ## update config
+        config["pathInfo"].update({
+            "chargeDir": chargeDir,
+        })
+
+        return config
+    
+    elif protocol == "RESP2":
+
+        ## directory to run ORCA QM calculations
+        solvatedDir: DirectoryPath = p.join(chargeDir, "RESP2_solvated")
+        gasPhaseDir: DirectoryPath = p.join(chargeDir, "RESP2_gas_phase")
+
+        ## update config
+        config["pathInfo"].update({
+            "chargeDir": chargeDir,
+            "solvatedDir": solvatedDir,
+            "gasPhaseDir": gasPhaseDir,
+        })
+
+        return config
---
+++ Laboratory/Experiments/Protocol_4_Charging/Charged_Monster.py
@@ -0,0 +1,296 @@
+
+## BASIC IMPORTS ##
+from os import path as p
+import os
+import glob
+from shutil import copy
+from subprocess import call, DEVNULL
+from pdbUtils import pdbUtils
+import pandas as pd
+import pexpect
+from tqdm import tqdm
+
+## drFrankenstein LIBRARIES ##
+from OperatingTools import drOrca
+from Protocol_1_Capping.Capping_Assistant import find_bonded_atoms
+
+## CLEAN CODE ##
+class FilePath:
+    pass
+class DirectoryPath:
+    pass
+
+# 🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲
+def run_orca_singlepoint_for_charge_calculations(args) -> None:
+    conformerXyz, orcaDir, fittingDir, chargeFittingInfo,  moleculeInfo, useSolvation = args
+    conformerName = p.basename(conformerXyz).split(".")[0]
+    
+    conformerQmDir = p.join(orcaDir, conformerName)
+    os.makedirs(conformerQmDir, exist_ok=True)
+
+    if useSolvation:
+        optSolvation = chargeFittingInfo["optSolvationMethod"]
+        singlePointSolvation = chargeFittingInfo["singlePointSolvationMethod"]
+    else: 
+        optSolvation = None
+        singlePointSolvation = None
+
+    orcaOptInput = drOrca.make_orca_input_for_opt(inputXyz = conformerXyz,
+                                                    outDir = conformerQmDir,
+                                                    moleculeInfo = moleculeInfo,
+                                                    qmMethod = chargeFittingInfo["optMethod"],
+                                                    solvationMethod = optSolvation)
+    
+    orcaOptOutput = p.join(conformerQmDir, "orca_opt.out")
+    if not p.isfile(orcaOptOutput):
+        drOrca.run_orca(orcaOptInput, orcaOptOutput)
+
+    optXyz = p.join(conformerQmDir, "orca_opt.xyz")
+    orcaSinglePointInput = drOrca.make_orca_input_for_singlepoint(inputXyz = optXyz,
+                                                                outDir = conformerQmDir,
+                                                                moleculeInfo = moleculeInfo,
+                                                                qmMethod = chargeFittingInfo["singlePointMethod"],
+                                                            solvationMethod = singlePointSolvation)
+    
+    orcaSinglePointOutput = p.join(conformerQmDir, "orca_sp.out")
+    if not p.isfile(orcaSinglePointOutput):
+        drOrca.run_orca(orcaSinglePointInput, orcaSinglePointOutput)
+
+    singlePointName = p.splitext(p.basename(orcaSinglePointInput))[0]
+    call(["orca_2mkl", p.join(conformerQmDir,singlePointName), "-molden"], stdout=DEVNULL)
+    singlePointMolden = p.join(conformerQmDir, "orca_sp.molden.input")
+
+    destMolden = p.join(fittingDir, f"{conformerName}.molden.input")
+
+    copy(singlePointMolden, destMolden)
+# 🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲
+def parse_multiwfn_output(rawMultiWfnOutputs: FilePath) -> pd.DataFrame:
+    """
+    Parses the output from Multiwfn to extract atomic charges.
+
+    Args:
+        rawMultiwfnOutputs (FilePath): Path to the file containing the raw Multiwfn output.
+
+    Returns:
+        chargesDf (pd.DataFrame) : A DataFrame containing columns 'atomIndex', 'atomElement', and 'Charge', 
+                      representing the index, element, and charge of each atom, respectively.
+
+    This function reads the Multiwfn output file, identifies the section after the 
+    "Successfully converged!" line, and extracts atomic charges until the line containing 
+    "Sum of charges:". The extracted data is structured into a DataFrame for further analysis.
+    """
+
+    with open(rawMultiWfnOutputs, 'r') as file:
+        lines = file.readlines()
+
+    # Find the index of the line after "Successfully converged!"
+    start_index = next(i for i, line in enumerate(lines) 
+                       if "Successfully converged!" in line) + 3
+
+    # Extract the charge data lines
+    chargeDataLines = []
+    for line in lines[start_index:]:
+        if "Sum of charges:" in line.strip():
+            break
+        chargeDataLines.append(line.strip())
+
+    # Parse the charge data into a DataFrame
+    data = []
+    for line in chargeDataLines:
+        parts = line.split(")")
+        charge = float(parts[1].strip())
+        parts = parts[0].split("(")
+        atomIndex = int(parts[0])
+        atomElement = parts[1].strip()
+
+        data.append({'atomIndex': atomIndex, 'atomElement': atomElement, 'Charge': charge})
+
+    chargesDf = pd.DataFrame(data)
+    return chargesDf
+# 🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲
+def get_charge_group_indexes(pdbFile: FilePath, config: dict) -> dict:
+    """
+    Uses user-defined charge groups to construct a complete dictionary of atom indexes for each charge group
+    Users do not need to specify hydrogen atoms, this function will find them automatically
+    All non-specified heavy atoms and associated protons are assigned to a "left-overs" charge group
+    Data is stored in the config dict and returned
+
+    Args:
+        pdbFile (FilePath): path to pdb file
+        config (dict): drFrankenstein config
+
+    Returns:
+        config (dict): updated config
+    """
+
+    pdbDf = pdbUtils.pdb2df(pdbFile)
+
+    chargeGroups = config["moleculeInfo"]["chargeGroups"]
+    overallCharge = config["moleculeInfo"]["charge"]
+
+    ## remove capping groups
+    uncappedDf = pdbDf[~pdbDf["RES_NAME"].isin(["NME", "ACE"])]
+
+    ## deal with user-defined charge groups
+    userDefinedAtoms = []
+    userDefinedCharge = 0
+    for chargeGroupName, chargeGroupData in chargeGroups.items():
+        ## get heavy atom names
+        heavyChargeGroupAtoms = chargeGroupData["atoms"]
+        ## fill in hydrogen atom names
+        protonNames = []
+        for heavyAtom in heavyChargeGroupAtoms:
+            boundProtons = find_bonded_atoms(pdbDf, heavyAtom)
+            protonNames.extend(boundProtons)
+        protonNames = [atomName for atomName in protonNames if atomName.startswith("H")] ##TODO: make this nicer
+        chargeGroupAtoms = heavyChargeGroupAtoms + protonNames
+        chargeGroupIndexes = uncappedDf[uncappedDf["ATOM_NAME"].isin(chargeGroupAtoms)]["ATOM_ID"].to_list()
+        chargeGroups[chargeGroupName]["indexes"] = chargeGroupIndexes
+        userDefinedCharge += chargeGroupData["charge"]
+        userDefinedAtoms.extend(chargeGroupAtoms)
+
+    ## deal with left-over atoms, these will all go in one group
+    leftOverDf = uncappedDf[~uncappedDf["ATOM_NAME"].isin(userDefinedAtoms)]
+    leftOverAtoms = leftOverDf["ATOM_NAME"].to_list()
+    leftOverIndexes = leftOverDf["ATOM_ID"].to_list()
+    leftOverCharge = overallCharge - userDefinedCharge 
+    ## deal with left-over atoms, these will all go in one group
+    leftOverDf = uncappedDf[~uncappedDf["ATOM_NAME"].isin(userDefinedAtoms)]
+    leftOverAtoms = leftOverDf["ATOM_NAME"].to_list()
+    leftOverIndexes = leftOverDf["ATOM_ID"].to_list()
+    leftOverCharge = overallCharge - userDefinedCharge 
+    
+    chargeGroups["left-over-atoms"] = {
+        "atoms" : leftOverAtoms,
+        "charge" : leftOverCharge,
+        "indexes" : leftOverIndexes
+    }
+    ## deal with Terminal Caps 
+    for capResName in ["NME", "ACE"]:
+        capDf = pdbDf[pdbDf["RES_NAME"]==capResName]
+        for capIndex, capResDf in capDf.groupby("RES_ID"):
+            chargeGroupAtoms = capResDf["ATOM_NAME"].to_list()
+            chargeGroupIndexes = capResDf["ATOM_ID"].to_list()
+            chargeGroupName = f"{capResName}_cap_{capIndex}"
+            chargeGroups[chargeGroupName] = {
+                "atoms": chargeGroupAtoms,
+                "charge": 0,
+                "indexes": chargeGroupIndexes
+            }
+
+    config["moleculeInfo"]["chargeGroups"] = chargeGroups
+
+    return config
+
+# 🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲
+def run_charge_fitting(config: dict,
+                        conformerListTxt: FilePath,
+                          chargeConstraintsTxt: FilePath,
+                            fittingDir: DirectoryPath) -> dict:
+    """
+    Runs MultiWFN to calculate charges for a set of conformers
+    Usually, MultiWFN_noGUI uses a silly command-line input style. 
+    This function uses pexpext to automate this for RESP charge fitting
+
+    Args:
+        config (dict): drFrankenstein config
+        conformerListTxt (FilePath): path to conformer list file
+        chargeConstraintsTxt (FilePath): path to charge constraints file
+        fittingDir (DirectoryPath): path to fitting directory
+    Returns:
+        config (dict): updated config
+    """
+
+    ## Unpack pathInfo
+    multiWfnDir = config["pathInfo"]["multiWfnDir"]
+    nConformers = config["chargeFittingInfo"]["nConformers"]
+
+    ## Change dir to MultiWFN build dir so it can find settings.ini
+    os.chdir(multiWfnDir)
+
+    ## Get MultiWFN binary
+    multiWfnExe = p.join(multiWfnDir, "Multiwfn_noGUI")
+    if not p.isfile(multiWfnExe):
+        raise FileNotFoundError("Multiwfn_noGUI not found")
+
+    ## Get a molden file for the input command
+    moldenFile = glob.glob(p.join(fittingDir, "*.molden.input"))[0]
+
+    ## Define output file
+    outputFile = p.join(fittingDir, "MultiWfn_raw_outputs.txt")
+
+    ## Open a file object for continuous logging
+    with open(outputFile, 'w') as logFile:
+        # Clear file and write a starting message
+        logFile.write("Starting charge fitting process...\n")
+        logFile.flush()  # Ensure it’s written immediately
+
+        ## Spawn the child process with logfile enabled
+        chargeFittingCommand = f"{multiWfnExe} {moldenFile}"
+        child = pexpect.spawn(chargeFittingCommand, encoding='utf-8', logfile=logFile)
+
+        ####### GET TO RESP MAIN MENU #######
+        child.expect(r'.*300.*\n?\r?')
+        child.sendline("7")
+
+        child.expect(r'.*20.*\n?\r?')
+        child.sendline("18")
+
+        ###### LOAD CONFORMERS #######
+        child.expect(r'.*11.*\n?\r?')
+        child.sendline("-1")
+
+        child.expect(r'.*Input.*\n?\r?')
+        child.sendline(conformerListTxt)
+
+        ####### LOAD CHARGE CONSTRAINTS #######
+        child.expect(r'.*11.*\n?\r?')
+        child.sendline("6")
+
+        child.expect(r'.*1.*\n?\r?')
+        child.sendline("1")
+
+        child.expect(r'.*Input.*\n?\r?')
+        child.sendline(chargeConstraintsTxt)
+
+        ####### RUN CALCULATIONS #######
+        child.expect(r'.*11.*\n?\r?')
+        child.sendline("2")
+
+        # Monitor the output for progress updates
+
+        # Initialize tqdm progress bar
+        tqdmBarOptions = {
+        "desc": f"Performing Charge Fitting",
+        "ascii": "-ϟ",  
+        "colour": "yellow",
+        "unit":  "calculations",
+        "dynamic_ncols": True,
+        "leave": True
+            }
+        totalTickProgress = nConformers * 100.00
+        progress_bar = tqdm(total=totalTickProgress, **tqdmBarOptions)
+        try:
+            while True:
+                # Expect either a progress line or the end of the process (EOF)
+                index = child.expect([r"Progress: \[.*?\]\s+(\d+\.\d+) %",
+                                      r" 11 Choose ESP type, current: Nuclear \+ Electronic[\n\r]?",
+                                      r".*\(y/n\)[\n\r]?", pexpect.TIMEOUT], 
+                                      timeout=5)
+                if index == 0:  # Progress line matched
+                    progress_bar.update(1)  # Update the tqdm bar by 1 tick
+                
+                elif index in [1, 2]:  # (process finished)
+                    break
+                
+                elif index == 3:  # Timeout (no output for 5 seconds)
+                    continue
+
+        except pexpect.ExceptionPexpect as e:
+            raise e
+
+        # Close the child process
+        child.close()
+
+    return outputFile
+# 🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲🗲
